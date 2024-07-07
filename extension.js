e6908f19188550e7bc79b5067d3c5fba97fa0bd6
zeghmouri <ma.zeghmouri@gmail.com>
2024-07-07 16:25:53 +0200

    V0

--- extension.js
+++ extension.js
@@ -1,7 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
+
+let resourcesFolder;
+let tempResourcesFolder;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -9,22 +14,152 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    const loadConfig = () => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) return;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-files-on-save" is now active!');
+        const config = context.workspaceState;
+        resourcesFolder = config.get('resourcesFolder', path.join(workspaceFolders[0].uri.fsPath, 'resources'));
+        tempResourcesFolder = config.get('tempResourcesFolder', path.join(workspaceFolders[0].uri.fsPath, 'temp_resources'));
+    };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('copy-files-on-save.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    const saveConfig = (resources, tempResources) => {
+        context.workspaceState.update('resourcesFolder', resources);
+        context.workspaceState.update('tempResourcesFolder', tempResources);
+        resourcesFolder = resources;
+        tempResourcesFolder = tempResources;
+    };
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Copy Files on Save!');
-	});
+    loadConfig();
 
-	context.subscriptions.push(disposable);
+    const copyFileOrDirectoryToTemp = (filePath) => {
+        if (filePath.startsWith(resourcesFolder)) {
+            const relativePath = path.relative(resourcesFolder, filePath);
+            const tempFilePath = path.join(tempResourcesFolder, relativePath);
+
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    vscode.window.showErrorMessage(`Failed to stat file: ${err.message}`);
+                    return;
+                }
+
+                if (stats.isDirectory()) {
+                    fs.mkdir(tempFilePath, { recursive: true }, (err) => {
+                        if (err) {
+                            vscode.window.showErrorMessage(`Failed to create directory: ${err.message}`);
+                        } else {
+                            vscode.window.showInformationMessage(`Directory created in temp_resources: ${relativePath}`);
+                        }
+                    });
+                } else {
+                    fs.mkdirSync(path.dirname(tempFilePath), { recursive: true });
+                    fs.copyFile(filePath, tempFilePath, (err) => {
+                        if (err) {
+                            vscode.window.showErrorMessage(`Failed to copy file: ${err.message}`);
+                        } else {
+                            vscode.window.showInformationMessage(`File copied to temp_resources: ${relativePath}`);
+                        }
+                    });
+                }
+            });
+        }
+    };
+
+    const deleteFileOrDirectoryFromTemp = (filePath) => {
+        if (filePath.startsWith(resourcesFolder)) {
+            const relativePath = path.relative(resourcesFolder, filePath);
+            const tempFilePath = path.join(tempResourcesFolder, relativePath);
+
+            fs.stat(tempFilePath, (err, stats) => {
+                if (err) {
+                    if (err.code !== 'ENOENT') {
+                        vscode.window.showErrorMessage(`Failed to stat temp file: ${err.message}`);
+                    }
+                    return;
+                }
+
+                if (stats.isDirectory()) {
+                    fs.rmdir(tempFilePath, { recursive: true }, (err) => {
+                        if (err) {
+                            vscode.window.showErrorMessage(`Failed to delete directory: ${err.message}`);
+                        } else {
+                            vscode.window.showInformationMessage(`Directory deleted from temp_resources: ${relativePath}`);
+                        }
+                    });
+                } else {
+                    fs.unlink(tempFilePath, (err) => {
+                        if (err && err.code !== 'ENOENT') {
+                            vscode.window.showErrorMessage(`Failed to delete file: ${err.message}`);
+                        } else {
+                            vscode.window.showInformationMessage(`File deleted from temp_resources: ${relativePath}`);
+                        }
+                    });
+                }
+            });
+        }
+    };
+
+    const renameFileOrDirectoryInTemp = (oldFilePath, newFilePath) => {
+        if (oldFilePath.startsWith(resourcesFolder)) {
+            const relativeOldPath = path.relative(resourcesFolder, oldFilePath);
+            const tempOldFilePath = path.join(tempResourcesFolder, relativeOldPath);
+            const relativeNewPath = path.relative(resourcesFolder, newFilePath);
+            const tempNewFilePath = path.join(tempResourcesFolder, relativeNewPath);
+
+            fs.rename(tempOldFilePath, tempNewFilePath, (err) => {
+                if (err) {
+                    vscode.window.showErrorMessage(`Failed to rename file or directory: ${err.message}`);
+                } else {
+                    vscode.window.showInformationMessage(`File or directory renamed in temp_resources: ${relativeNewPath}`);
+                }
+            });
+        }
+    };
+
+    let disposableSave = vscode.workspace.onDidSaveTextDocument((document) => {
+        const filePath = document.uri.fsPath;
+        copyFileOrDirectoryToTemp(filePath);
+    });
+
+    let disposableCreate = vscode.workspace.onDidCreateFiles((event) => {
+        for (const file of event.files) {
+            const filePath = file.fsPath;
+            copyFileOrDirectoryToTemp(filePath);
+        }
+    });
+
+    let disposableDelete = vscode.workspace.onDidDeleteFiles((event) => {
+        for (const file of event.files) {
+            const filePath = file.fsPath;
+            deleteFileOrDirectoryFromTemp(filePath);
+        }
+    });
+
+    let disposableRename = vscode.workspace.onDidRenameFiles((event) => {
+        for (const { oldUri, newUri } of event.files) {
+            const oldFilePath = oldUri.fsPath;
+            const newFilePath = newUri.fsPath;
+            renameFileOrDirectoryInTemp(oldFilePath, newFilePath);
+        }
+    });
+
+    let setPathsCommand = vscode.commands.registerCommand('fileSyncOnSave.setPaths', async () => {
+        const resourcesPath = await vscode.window.showInputBox({ prompt: 'Enter the path to the resources folder' });
+        const tempResourcesPath = await vscode.window.showInputBox({ prompt: 'Enter the path to the temp resources folder' });
+
+        if (resourcesPath && tempResourcesPath) {
+            saveConfig(resourcesPath, tempResourcesPath);
+            vscode.window.showInformationMessage('Paths updated successfully.');
+        } else {
+            vscode.window.showErrorMessage('Paths not updated. Both paths must be provided.');
+        }
+    });
+
+    context.subscriptions.push(disposableSave);
+    context.subscriptions.push(disposableCreate);
+    context.subscriptions.push(disposableDelete);
+    context.subscriptions.push(disposableRename);
+    context.subscriptions.push(setPathsCommand);
 }
 
 // This method is called when your extension is deactivated
